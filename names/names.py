6bd9c40f763429f0fadc2775791eeeebfde2cc02
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-06-14 14:06:56 -0400

    Attempts to optimize names.py code

--- names/names.py
+++ names/names.py
@@ -2,21 +2,29 @@ import time
 
 start_time = time.time()
 
-f = open('names_1.txt', 'r')
-names_1 = f.read().split("\n")  # List containing 10000 names
-f.close()
+# f = open('names_1.txt', 'r')
+# names_1 = f.read().split("\n")  # List containing 10000 names
+# f.close()
 
-f = open('names_2.txt', 'r')
-names_2 = f.read().split("\n")  # List containing 10000 names
-f.close()
+# f = open('names_2.txt', 'r')
+# names_2 = f.read().split("\n")  # List containing 10000 names
+# f.close()
 
-duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# duplicates = []
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+from collections import Counter
+duplicates = {}
+with open('names_1.txt') as file:
+    c = Counter(part for line in file for part in line.split() if part in duplicates)
+    
+with open('names_1.txt') as file:
+    print(sum(duplicates.get(part, 0) for line in file for part in line.split()))
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+# print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+# print (f"runtime: {end_time - start_time} seconds")
 
