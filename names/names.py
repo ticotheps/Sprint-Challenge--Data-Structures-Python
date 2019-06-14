69aea1872a9ae0b318b5c34f1808546d89d6f80c
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-06-14 15:47:53 -0400

    Creates 1st and 2nd iteration of original solution code in names.py file

--- names/names.py
+++ names/names.py
@@ -2,6 +2,8 @@ import time
 
 start_time = time.time()
 
+#--------------------------ORIGINAL SOLUTION-----------------------------
+
 # f = open('names_1.txt', 'r')
 # names_1 = f.read().split("\n")  # List containing 10000 names
 # f.close()
@@ -16,15 +18,35 @@ start_time = time.time()
 #         if name_1 == name_2:
 #             duplicates.append(name_1)
 
-from collections import Counter
-duplicates = {}
-with open('names_1.txt') as file:
-    c = Counter(part for line in file for part in line.split() if part in duplicates)
+#-----------------------------ITERATION #1-------------------------------
+
+duplicates = []
+
+with open('names_1.txt') as file1, open('names_2.txt') as file2:
+    for line in set(line.strip() for line in file1) & set(line.strip() for line in file2):
+        if line: 
+            duplicates.append(line)
+
+#-----------------------------ITERATION #2-------------------------------
+
+# with open('names_1.txt', 'r') as file1:
+#     with open('names_2.txt', 'r') as file2:
+#         duplicate = set(file1).intersection(file2)
+        
+# duplicate.discard('\n')
     
-with open('names_1.txt') as file:
-    print(sum(duplicates.get(part, 0) for line in file for part in line.split()))
+# with open('duplicates.txt', 'w') as file_out:
+#     for line in duplicate:
+#         file_out.write(line)
+        
+# f = open('duplicates.txt', 'r')
+# duplicates = f.read().split("\n")  # List containing 10000 names
+# f.close()
+
+#-----------------------------TEST CASES---------------------------------
 
 end_time = time.time()
-# print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-# print (f"runtime: {end_time - start_time} seconds")
+
+print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print (f"runtime: {end_time - start_time} seconds")
 
